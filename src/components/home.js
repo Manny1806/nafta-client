7b6977b71dc0e7fe058fc3d6482c417e97d8d4fa
Alex Widner <manny1806@gmail.com>
2018-11-10 15:56:17 -0800

    added router and provider

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import logo from './logo.svg';
-import './App.css';
+import './home.css';
 
-class App extends Component {
+class Home extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="Home">
+        <header className="Home-header">
           
         </header>
       </div>
@@ -16,4 +15,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Home;
