4a98421085a7a7472369a3af1c084c949af8620c
Alex Widner <manny1806@gmail.com>
2018-12-02 23:37:09 -0800

    basic edit and add of pro posts. needs some fixing though

--- src/actions/home/pro-actions.js
+++ src/actions/home/pro-actions.js
@@ -14,9 +14,39 @@ export const fetchProPostsSuccess = (proPosts) =>({
 export const ADD_EMPTY_PRO_ENTRY = 'ADD_EMPTY_PRO_ENTRY'
 export const addEmptyProEntry = () =>({
     type: ADD_EMPTY_PRO_ENTRY,
-    proPost: {title: "1",
-              quote: "1",
+    proPost: {title: "",
+              quote: "",
               new: true}
+})
+
+export const EDIT_PRO_POST_REQUEST = 'EDIT_PRO_POST_REQUEST'
+export const editProPostRequest = () =>({
+    type: EDIT_PRO_POST_REQUEST
+})
+
+export const EDIT_PRO_POST_SUCCESS = 'EDIT_PRO_POST_SUCCESS'
+export const editProPostSuccess = () => ({
+    type: EDIT_PRO_POST_SUCCESS
+})
+
+export const ADD_PRO_POST_REQUEST = 'ADD_PRO_POST_REQUEST'
+export const addProPostRequest = () =>({
+    type: ADD_PRO_POST_REQUEST
+})
+
+export const ADD_PRO_POST_SUCCESS = 'ADD_PRO_POST_SUCCESS'
+export const addProPostSuccess = () => ({
+    type: ADD_PRO_POST_SUCCESS
+})
+
+export const DELETE_PRO_POST_REQUEST = 'DELETE_PRO_POST_REQUEST'
+export const deleteProPostRequest = () =>({
+    type: DELETE_PRO_POST_REQUEST
+})
+
+export const DELETE_PRO_POST_SUCCESS = 'DELETE_PRO_POST_SUCCESS'
+export const deleteProPostSuccess = () => ({
+    type: DELETE_PRO_POST_SUCCESS
 })
 
 export const fetchProPosts = () => dispatch =>{
@@ -26,4 +56,39 @@ export const fetchProPosts = () => dispatch =>{
     .then(res => {
         console.log(res._embedded.pro)
         dispatch(fetchProPostsSuccess(res._embedded.pro))})
+}
+
+export const editProPost = (id, values) => dispatch => {
+    dispatch(editProPostRequest())
+    fetch(`${API_BASE_URL}pro/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify(values),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => !res.ok ? Promise.reject(res.statusText) : undefined)
+    .then(res => dispatch(editProPostSuccess()))
+    .then(res => dispatch(fetchProPosts()))
+}
+
+export const addProPost = (values) => dispatch => {
+    dispatch(addProPostRequest())
+    fetch(`${API_BASE_URL}pro/`, {
+        method: 'POST',
+        body: JSON.stringify(values),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => !res.ok ? Promise.reject(res.statusText) : undefined)
+    .then(res => dispatch(addProPostSuccess()))
+    .then(res => dispatch(fetchProPosts()))
+}
+
+export const deleteProPost = (id) => dispatch => {
+    dispatch(deleteProPostRequest())
+    fetch(`${API_BASE_URL}pro/${id}`, {
+        method: 'DELETE'
+    }).then(res => !res.ok ? Promise.reject(res.statusText) : undefined)
+    .then(res => dispatch(deleteProPostSuccess()))
+    .then(res => dispatch(fetchProPosts()))
 }
--- src/reducers/home/pro-reducers.js
+++ src/reducers/home/pro-reducers.js
@@ -1,7 +1,13 @@
 import {
     PRO_POSTS_REQUEST,
     PRO_POSTS_SUCCESS,
-    ADD_EMPTY_PRO_ENTRY
+    ADD_EMPTY_PRO_ENTRY,
+    EDIT_PRO_POST_REQUEST,
+    EDIT_PRO_POST_SUCCESS,
+    ADD_PRO_POST_REQUEST,
+    ADD_PRO_POST_SUCCESS,
+    DELETE_PRO_POST_REQUEST,
+    DELETE_PRO_POST_SUCCESS
 }   from '../../actions/home/pro-actions'
 
 const initialState = {
@@ -14,6 +20,18 @@ export default function reducer(state = initialState, action) {
     }
     else if (action.type === PRO_POSTS_SUCCESS) {
         return {...state, proPosts: action.proPosts}
+    }
+    else if (action.type === EDIT_PRO_POST_REQUEST) {
+        return {...state}
+    }
+    else if (action.type === EDIT_PRO_POST_SUCCESS) {
+        return {...state}
+    }
+    else if (action.type === ADD_PRO_POST_REQUEST) {
+        return {...state}
+    }
+    else if (action.type === ADD_PRO_POST_SUCCESS) {
+        return {...state}
     }
     else if (action.type === ADD_EMPTY_PRO_ENTRY) {
         return {...state, proPosts: [action.proPost].concat(state.proPosts)}
