a5c2aff8d0c1ff9a4659c6ee0c0cdd42bd595b76
Alex <alex@localhost.localdomain>
2019-01-26 00:08:16 -0800

    set heroku server url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://localhost:8080';
-// export const API_BASE_URL = 'https://limitless-castle-95339.herokuapp.com/';
+// export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://nafta-reactor-server.herokuapp.com';
